3b73511fc3fc13ede4ab72093e1184e686445449
ishajha617 <ishajha617@gmail.com>
2025-10-29 14:29:07 +0530

    Added Azure Storage Account resource

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "my-simple-rg"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mytfstorage${random_integer.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
