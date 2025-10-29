e7f68a8ce75b30b790bd710ecbaa2ef4c460f18a
ishajha617 <ishajha617@gmail.com>
2025-10-29 16:44:45 +0530

    Fix: changed resource group name to avoid duplicate

--- main.tf
+++ main.tf
@@ -14,9 +14,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "my-simple-rg"
+  name     = "my-simple-rg-01"
   location = "East US"
 }
+
 resource "azurerm_storage_account" "storage" {
   name                     = "mytfstorage${random_integer.rand.result}"
   resource_group_name      = azurerm_resource_group.rg.name
