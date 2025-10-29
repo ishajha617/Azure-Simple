d51ffdfd146f1d4de26714fb9d6821255161d079
ishajha617 <ishajha617@gmail.com>
2025-10-29 14:39:29 +0530

    Added Azure Virtual Network and Subnet

--- main.tf
+++ main.tf
@@ -17,3 +17,31 @@ resource "azurerm_resource_group" "rg" {
   name     = "my-simple-rg"
   location = "East US"
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = "mytfstorage${random_integer.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
